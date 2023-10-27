0306b0023d570a24a87801895983245179ea73a2
unknown <amamdik@gmail.com>
2023-10-27 23:39:06 +0530

    pos system content updated

--- assignment/JS/InMemoryPOS/db/DB.js
+++ assignment/JS/InMemoryPOS/db/DB.js
@@ -11,3 +11,22 @@ var itemDB = [
     {code: "I00-003", itemName: "Sugar", qtyOnHand: 1, unitPrice: 249.00},
     {code: "I00-004", itemName: "soap", qtyOnHand: 20, unitPrice: 180.00}
 ];
+
+var orderDB = [
+    {
+        orderID: "000-001",
+        date: "2023/10/07",
+        customer: customerDB[0],
+        cart: [{item: itemDB[0], qty: 2}, {item: itemDB[1], qty: 1}],
+        discount: 250,
+        total: 10000,
+    },
+    {
+        orderID: "000-002",
+        date: "2023/11/01",
+        customer: customerDB[1],
+        cart: [{item: itemDB[2], qty: 1}, {item: itemDB[1], qty: 1}, {item: itemDB[0], qty: 1}],
+        discount: 10,
+        total: 3000
+    }
+];
