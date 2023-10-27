00da5f0294086d46ac3aa77e8b01402d0427e105
unknown <amamdik@gmail.com>
2023-10-27 23:26:08 +0530

    pos system content updated

--- assignment/JS/InMemoryPOS/db/DB.js
+++ assignment/JS/InMemoryPOS/db/DB.js
@@ -4,3 +4,10 @@ var customerDB = [
     {id: "C00-003", name: "Monashi Pahasi", address: "Panadura", salary: 50000},
     {id: "C00-004", name: "Amal Weerasekara", address: "Mathara", salary: 100000}
 ];
+
+var itemDB = [
+    {code: "I00-001", itemName: "Rice", qtyOnHand: 50, unitPrice: 210.00},
+    {code: "I00-002", itemName: "Dhal", qtyOnHand: 5, unitPrice: 345.00},
+    {code: "I00-003", itemName: "Sugar", qtyOnHand: 1, unitPrice: 249.00},
+    {code: "I00-004", itemName: "soap", qtyOnHand: 20, unitPrice: 180.00}
+];
