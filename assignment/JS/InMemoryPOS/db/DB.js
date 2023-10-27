e671dabc19b09a34227c735c28b1b4d7f1f9bd67
unknown <amamdik@gmail.com>
2023-10-27 23:19:26 +0530

    pos system content updated

---
+++ assignment/JS/InMemoryPOS/db/DB.js
@@ -0,0 +1,6 @@
+var customerDB = [
+    {id: "C00-001", name: "Amandi Keshala", address: "Galle", salary: 300000},
+    {id: "C00-002", name: "Navoda Reshan", address: "Colombo", salary: 500000},
+    {id: "C00-003", name: "Monashi Pahasi", address: "Panadura", salary: 50000},
+    {id: "C00-004", name: "Amal Weerasekara", address: "Mathara", salary: 100000}
+];
